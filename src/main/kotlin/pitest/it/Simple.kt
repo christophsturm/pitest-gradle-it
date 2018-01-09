e18910ecd8d43fb919a0488b4412c1b3a6833f16
Christoph Sturm <me@christophsturm.com>
2018-01-09 18:51:26 +0100

    initial

---
+++ src/main/kotlin/pitest/it/Simple.kt
@@ -0,0 +1,5 @@
+package pitest.it
+
+class Simple {
+    fun add(a:Int, b:Int) = a+b
+}
---
+++ src/test/kotlin/pitest/it/SimpleTest.kt
@@ -0,0 +1,11 @@
+package pitest.it
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SimpleTest {
+    @Test
+    fun `can add`() {
+        assertEquals(2, Simple().add(1,1))
+    }
+}
